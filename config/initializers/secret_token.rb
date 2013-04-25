d6428f2ced2417c78bd3cb8018d9d78b5b1d512f
joshdchap <toroid.x@gmail.com>
2013-04-25 19:08:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TomApp::Application.config.secret_token = '12420d6fdcd4d780fb81ee7937b5fb03d6ceeed1b974866e86e0cfc294ae439fad4e9ebb95fe8071c3fc0c449a31d1ba6bac6bd7787e4a3c8fd58a53cc394353'
