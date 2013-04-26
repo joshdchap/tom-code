a152c68f44a7c7e5d47791e2d02302133150d1c6
joshdchap <toroid.x@gmail.com>
2013-04-26 03:38:36 -0400

    Lonely Agents and Tests

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,11 @@
 class Contact < ActiveRecord::Base
 
-  CONTACT_TYPES = ["Client", "Prospect", "Referral Partner"]
+  CLIENT = "Client"
+  PROSPECT = "Prospect"
+  REFERRAL_PARTNER = "Referral Partner"
+  CONTACT_TYPES = [CLIENT, PROSPECT, REFERRAL_PARTNER]
 
+  has_many :cobrand_contacts, :class_name => "Contact", :foreign_key => "cobrand_id"
   belongs_to :cobrand, :class_name => "Contact"
 
   validates_presence_of :firstname
@@ -10,5 +14,22 @@ class Contact < ActiveRecord::Base
   validates :contact_type, :inclusion => { :in => CONTACT_TYPES,
     :message => "%{value} is not a valid type"}
 
+  validate :cobrands_can_only_be_referral_partners
+
+  scope :referral_partners, where(:contact_type => REFERRAL_PARTNER)
+
+  def self.lonely_agents
+   referral_partners.select{|t| t if t.cobrand_contacts.blank?}
+  end
+
+  def cobrands_can_only_be_referral_partners
+    if !cobrand.blank? && cobrand.contact_type != REFERRAL_PARTNER
+      errors.add(:cobrand, "Can only be of type #{REFERRAL_PARTNER}")
+    end
+  end
+
+  def name 
+    self.firstname + " " + self.lastname
+  end
 
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -8,25 +8,51 @@ describe Contact do
   it "is invalid without a firstname" do
     FactoryGirl.build(:contact, firstname: nil).should_not be_valid
   end
+
   it "is invalid without a lastname" do
     FactoryGirl.build(:contact, lastname: nil).should_not be_valid
   end
-  it "is invalid with an improper cobrand"
+
+  it "is invalid with an improper cobrand" do
+    client = FactoryGirl.create(:contact, contact_type: "Client")
+    client2 = FactoryGirl.build(:contact, contact_type: "Client", 
+                 cobrand: client).should_not be_valid
+  end
+
   it "is invalid without a type" do
     FactoryGirl.build(:contact, contact_type: nil).should_not be_valid
   end
+
+  it "returns full name" do 
+    FactoryGirl.build(:contact).name.should == "Josh Chapman"
+  end
+
   describe "cobrand association" do
 
     before :each do
-      @miller = FactoryGirl.create(:contact, lastname: "Miller")
+      @miller = FactoryGirl.create(:contact, lastname: "Miller",
+                  contact_type: Contact::REFERRAL_PARTNER)
       @rogers = FactoryGirl.create(:contact, lastname: "Rogers", cobrand: @miller)
-      @chapman = FactoryGirl.create(:contact, lastname: "Chapman")
+      @chapman = FactoryGirl.create(:contact, lastname: "Chapman",
+                  contact_type: Contact::REFERRAL_PARTNER)
+    end
+    context "setting and getting" do 
+      it { should have_many(:cobrand_contacts) }
+      it { should belong_to(:cobrand) }
+
+      it "sets the cobrand properly" do
+        @rogers.cobrand.should == @miller
+      end
+
+      it "finds the cobrand contacts properly" do
+        @miller.cobrand_contacts.should == [@rogers]
+      end
     end
 
-    it { should belong_to(:cobrand) }
-
-    it "sets the cobrand properly" do
-      @rogers.cobrand.should == @miller
+    context "lonely agent scope" do
+      it "returns lonely agents" do
+        Contact.lonely_agents.should == [@chapman]
+      end
     end
   end
 end
